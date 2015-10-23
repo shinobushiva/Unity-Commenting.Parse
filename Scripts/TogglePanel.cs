294bb78a4a2cb6f73d4b1707ccdf7090f77dcfa0
Shinobu Izumi <izumi@cis.sojo-u.ac.jp>
2015-10-23 18:15:26 +0900

    first

---
+++ Scripts/TogglePanel.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+public class TogglePanel : MonoBehaviour {
+
+	public Toggle prefab;
+	public string[] options;
+	private bool[] optionsSelected;
+
+	// Use this for initialization
+	void Start () {
+		Toggle[] toggles = GetComponentsInChildren<Toggle> ();
+		foreach (Toggle t in toggles) {
+			DestroyImmediate(t.gameObject);
+		}
+
+		optionsSelected = new bool[options.Length];
+
+		int count = 0;
+		foreach (string op in options) {
+			Toggle t = Instantiate<Toggle> (prefab);
+			t.isOn = false;
+			t.GetComponentInChildren<Text>().text = op;
+			int c = count;
+			t.onValueChanged.AddListener((val) => {
+				optionsSelected[c] = val;
+			});
+			t.transform.SetParent(transform, false);
+			count++;
+		}
+	}
+
+	public string[] SelectedOptions(){
+		List<string> list = new List<string> ();
+		for (int i=0; i<options.Length; i++) {
+			if(optionsSelected[i]){
+				list.Add (options[i]);
+			} 
+		}
+		return list.ToArray();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ UserDetailAddPanel.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using Parse;
+using System;
+
+public class UserDetailAddPanel : UserAddPanel
+{
+
+
+	public Dropdown age;
+	public Dropdown gender;
+	public Dropdown occupation;
+	public Dropdown residence;
+	public Dropdown freaquency;
+	public TogglePanel transports;
+
+	// Use this for initialization
+	void Start ()
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	
+	}
+
+	public override void AddUser (ParseDatastoreMaster pdm)
+	{
+		pdm.Dismiss ();
+		
+		ParseUser user;
+
+		if (email.text.Length > 0) {
+			user = new ParseUser ()
+			{
+				Username = username.text,
+				Password = password.text,
+				Email = email.text
+			};
+		} else {
+			user = new ParseUser ()
+			{
+				Username = username.text,
+				Password = password.text,
+			};
+		}
+		user ["age"] = age.options [age.value].text;
+		user ["gender"] = gender.options [gender.value].text;
+		user ["occupation"] = occupation.options [occupation.value].text;
+		user ["residence"] = residence.options [residence.value].text;
+		user ["freaquency"] = freaquency.options [freaquency.value].text;
+		user ["transports"] = transports.SelectedOptions ();
+		
+		
+		user.SignUpAsync ().ContinueWith (t =>
+		{
+			if (t.IsFaulted || t.IsCanceled) {
+				// The login failed. Check the error to see why.
+				print ("signup failed");
+				print (t.Exception);
+
+				pdm.updateLoginStateFlag = true;
+			} else {
+				// Login was successful.
+				print ("signup success");
+				pdm.updateLoginStateFlag = true;
+			}
+		});
+	}
+
+
+
+}
